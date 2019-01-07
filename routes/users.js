cdbf55119783b2e7bcee34470332e0b74b35c26a
Sobhit Kumar <sobhit2010@gmail.com>
2019-01-07 17:29:17 +0530

    minor fixes

--- routes/users.js
+++ routes/users.js
@@ -1,69 +1,68 @@
 const express = require('express');
 const router = express.Router();
-const passport=require('passport');
-const jwt=require('jsonwebtoken');
-const config=require('../config/database');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
-const User=require('../models/user');
+const User = require('../models/user');
 
 //Register
-router.post('/register',(req,res,next)=>{
-let newUser=new User({
-    name:req.body.name,
-    email:req.body.email,
-    username:req.body.username,
-    password:req.body.password
-});
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
 
-User.addUser(newUser,(err,user)=>{
-if(err)
-{
-    res.json({success:false,msg:'Failed to register user'});
-}else{
-    res.json({success:true,msg:'register user successfully'});
-}
-});
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to register user' });
+        } else {
+            res.json({ success: true, msg: 'register user successfully' });
+        }
+    });
 });
 
 
 //Authenticate
-router.post('/authenticate',(req,res,next)=>{
-    const username=req.body.username;
-    const password=req.body.password;
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-    User.getUserByUsername(username,(err,user)=>{
-        if(err) throw err;
-        if(!user){
-            return res.json({success:false,msg:'User Not found'});
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User Not found' });
         }
-        User.comparePassword(password,user.password,(err,isMatch)=>{
-            if(err) throw err;
-            if(isMatch){
-                    const token=jwt.sign(user, config.secret,{
-                        expiresIn:604800 //1 week
-                    });
-                    res.json({
-                        success:true,
-                        token:'JWT'+token,
-                        user:{
-                            id:user._id,
-                            name:user.name,
-                            username:user.username,
-                            email:user.email
-                        }
-                    });
-            }else{
-                return res.json({success:false,msg:'Wrong Password'});
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                const token = jwt.sign({ data: user }, config.secret, {
+                    expiresIn: 604800 //1 week
+                });
+                res.json({
+                    success: true,
+                    token: 'JWT' + token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({ success: false, msg: 'Wrong Password' });
             }
         });
     });
-    });
+});
 
 
 //Profile
-router.get('/profile',(req,res,next)=>{
+router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
-    });
+});
 
 
 
